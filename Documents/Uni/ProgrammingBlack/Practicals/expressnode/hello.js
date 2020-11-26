7d51838dfb96002c3eb2486c95ce8472f5e4690b
Seb Dodds <tntn75@durham.ac.uk>
2020-11-26 11:38:34 +0000

    Finished part 3

--- Documents/Uni/ProgrammingBlack/Practicals/expressnode/hello.js
+++ Documents/Uni/ProgrammingBlack/Practicals/expressnode/hello.js
@@ -5,4 +5,20 @@ app.get('/', function(req, resp){
    resp.send('Hello world')
 })
 
+app.get('/waves/:word',function(req,resp){
+    word = (req.params.word)
+    console.log('Waving at ' + word)
+    resp.send('' + word)
+})
+app.get('/secret',function(req,resp){
+    resp.send('SHHHHHH')
+})
+
+app.get('/random/:max', function(req, resp){
+    max = parseInt(req.params.max)
+    rand = Math.floor(Math.random()*max) +1
+    console.log('Max via url is ' + max + ' rand is ' + rand)
+    resp.send('' + rand)
+  })
+
 app.listen(8090)
