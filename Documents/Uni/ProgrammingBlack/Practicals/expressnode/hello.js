d811ed6323b7a6a94e291a3eee672b489c396d13
G4FCollingwood <tntn75@durham.ac.uk>
2020-12-03 10:25:44 +0000

    Finished Part 3 Finished Part 3 Finished Part 4 Finished Part 4

--- Documents/Uni/ProgrammingBlack/Practicals/expressnode/hello.js
+++ Documents/Uni/ProgrammingBlack/Practicals/expressnode/hello.js
@@ -5,4 +5,26 @@ app.get('/', function(req, resp){
    resp.send('Hello world')
 })
 
+app.get('/waves/:word',function(req,resp){
+    word = (req.params.word)
+    console.log('Waving at ' + word)
+    resp.send('' + word)
+})
+app.get('/secret',function(req,resp){
+    resp.send('SHHHHHH')
+})
+
+app.get('/random/:max', function(req, resp){
+    max = parseInt(req.params.max)
+    rand = Math.floor(Math.random()*max) +1
+    console.log('Max via url is ' + max + ' rand is ' + rand)
+    resp.send('' + rand)
+  })
+
+app.get('/w', function(req, resp){
+    randomperson = req.query.person
+    console.log('The random person is ' + randomperson)
+    resp.send('' + randomperson)
+})
+  
 app.listen(8090)
